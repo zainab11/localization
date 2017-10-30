a99358cc8e03f070163a465e8d88174e1d0c2ec2
mommand <zmomand@technation.af>
2017-10-30 13:38:27 +0430

    First commit

---
+++ site.php
@@ -0,0 +1,239 @@
+<?php
+
+return [
+
+    /* all site location varaible */
+    // menu options 
+    'home'          => 'Home',
+    'highlights'    => 'Highlights',
+    'courses'       => 'Courses',
+    'enroll'        => 'Enroll',
+    'certification' => 'Certification',
+    'instruction'   => 'Instruction',
+    'contact'       => 'Contact',
+    'click_for_admission' => 'Click For Admission',
+    'enroll_details' => 'TechLiteracy provides hundreds of digital literacy courses you can enroll. Please, fill out course enrollment form below to attend your desired courses. Our courses are cost-effective and are taught by highly qualified instructors using advanced methodology and state-of-the-art technologies.',
+    'about_us'  => 'About Us',
+    'about_details'  => 'TechLiteracy is a Digital Literacy Campaign, across female schools in Afghanistan. The aim of TechLiteracy is to bring resources for women and girls to gain new skills and become employable and productive members of the digital world-- digital literacy serves as the backbone of e-governance. 
+
+        Participants of the campaign will include female citizens, aged 15-35, with little to no experience in computing or digital literacy. Through engaging with us the participants will develop a fundamental understanding of digital technologies (computers, mobile, hardware, software, Internet, tools and applications). This will be done through training, coaching and education programs across schools in Afghanistan. 
+
+        TechLiteracy is managed by TechNation, a Kabul-based company focusing on technology, innovation, education and community. TechNation is invites diverse stakeholders to partner in the program design and implementation.',
+    'background'        => 'Background',
+    'team'  =>          'Team',
+    'team_details'      => 'TechLiteracy is managed by top-notch national and international industry leaders, educationists, technology professionals, and government officials who have many years of experience in education, policy, technology, management and leadership.',
+    'streeing_commitee'     => 'Steering Committee',
+    'partners'  =>      'Partners',
+    'prgram_by' =>      'TechLiteracy is program by:',
+    'technation_intro'  => 'intro about technation',
+    'in_partnership_with'       => 'in Partnership with',
+    'teaching_method'           => 'Teaching Methodology',
+    'methology_details'         => 'The TechLiteracy program’s digital competencies will include essential skills development in:',
+    'method1'            => 'Information Management',
+    'method2'            => 'Communication and  Collaboration',
+    'method3'            => 'Digital Media',
+    'method4'            => 'Using Digital Tools for Learning',
+    'method5'            => 'Management of the Internet',
+    'after_method_text1'  => 'These competencies will enable learners with the means to access, evaluate and analyse and hence make an informed choice from a range of available data and information sources. It will enable learners to learn to communicate, collaborate and network with others. It will enhance their skills to analyse messages mediated by digital media, and will enable them to express themselves creatively across a range of digital media. Participants will gain competences in using digital tools in various media and in different modes of learning (autonomous, collaborative, exploratory, designing). The skills TechLiteracy will be teaching are necessary to become sophisticated users of  computers, essential tools and applications, and online resources.',
+    'after_method_text2'  => 'Gaining these skills and competencies will help the program participants to become productive at home and at work. They will learn how to be responsible and competent in managing the internet, how to stay safe and secure online, how to make informed choices over privacy, how to respect intellectual property, what it takes to abide by terms and conditions of systems they use and respect the rights and feelings of others, and know how to use technology to complement work and lifestyle. They will be able to consider careers where one can put their skills to work.',
+
+    'latest_courses' => 'Latest Courses',
+    'last_name'         => 'Last Name',
+    'first_name'        => 'First Name',
+    'id_type'           => 'ID Type',
+    'id'                => 'ID',
+    'select_course'     => "Select Course",
+    'fees_to_be_paid'   => "Total fees be paid",
+    'payment_details'   => "Payment Details",
+    'dob'               => "Date of Birth",
+    'email'             => "Email",
+    'phone'             => "Phone",
+    'address'           => "Address",
+    'emergency_contact' => "Emergency Contact",
+    'education_background' => "Education Background",
+    'dg_background'        => "Computer/Digital Literacy Background",
+    'language'             => "Language Proficiency",
+    'alternative_phone'    => 'Alternative Phone',
+    'statment_of_purpose'  => 'statement of purpose',
+    'institute_name'       => 'Institute Name',
+    'your_contribution'    => 'Student Contribution',
+    // WIE application form options translation
+    'about_wie'             => '',
+    'personal_information'  => 'Personal Information',
+    'full_name'             => 'Full Name',
+    'father_name'           => 'Fathers Name',
+    'gender'                => 'Gender',
+    'male'                  => 'Male',
+    'female'                => 'Female',
+    'age'                   => 'Age',
+    'email_address'         => 'Email Address',
+    'mobile_cell_phone'     => 'Mobile/Cell Phone',
+    'wie_notice'            => 'Women in the Economy is only accepting registration forms from Afghan women between the ages 16-30 at this time',
+    'education_and_job_search'  => 'Education and Job Search',
+    'current_student'           => 'Are you currently a student',
+    'highest_level_of_education' => 'What is the highest level of education you have completed?',
+    'levels_of_education'        => 'Level of Education',
+    'elementary'                 => 'Elementary (grades 0-5)',
+    'middle_school'              => 'Middle School (grades 6-8)',
+    'high_school'                => 'High School (grades 9-12)',
+    'university'                 => 'University (bachelor)',
+    'master'                     => 'Master',
+    'phd'                        => 'phD',
+    'other_education'            => 'Have you graduated from other type of educational program? If, yes please check any other educational program that applies: If no, skip the next',
+    'business_private_company'   => 'Business / Private Company',
+    'tvet'                       => 'TVET (institute)',
+    'employer_on_the_job_training' => 'Employer on the job training',
+    'other'                        => 'Other',
+    'notice2'                      => 'for all checked boxes, specify type of program - (certificate, diploma, other) and focus area.',
+    // why are you registering with women in the economy section options
+    'why_you_register'             => 'Why are you registering with Women in the Economy? ',
+    'find_a_new_job'               => 'to find a new job',
+    'internship'                   => 'to participate in an internship/apprenticeship program',
+    'participate_training'         => 'To participate in training to advance my career',
+    'scholarship'                  => 'To apply for a scholarship',
+    // types of job options
+    'what_type_of_job'             => 'What type of job are you interested in? (please check all that apply)',
+    'full_time'                    => 'Full Time',
+    'part_time'                    => 'Part Time',
+    'self_employed'                => 'Self-Employed/entrepreneur',
+    'internship_office'            => 'Internship (office)',
+    'temporary'                    => 'Seasonal/temporary',
+    'apprenticeship'               => 'Apprenticeship(trade)',
+    'volunteer'                    => 'Volunteer / Community Services',
+    //level of job options
+    'what_level_of_job'            => 'What level of job best matches your experience and skills?',
+    'entry_level'                  => 'Entry Level (0 years)',
+    'junior'                       => 'Junior (1-3 Years)',
+    'mid_level'                    => 'Mid_level(4-5 Years)',
+    'senior'                       => 'Senior (5+ Years)',
+    // when would you available to start work (options)
+    'when_start_work'              => 'When would you be available to start work?',
+    'three_months'                 => '1-3 Months',
+    'four_months'                  => '4-6 Months',
+    'one_year'                     => 'Within one Year',
+    'other-date'                   => 'If other date (specify)',
+    // How many years do you plan to work (options)
+    'work_plan_time'               => 'How many years do you plan to work?',
+    'less_than_one_year'           => 'Less than one year',
+    'two_years'                    => '2-3 Years',
+    'four_years'                   => '4-5 Years',
+    'six_years'                    => '6-7 Years',
+    'seven_years'                  => '7+ Years',
+    'family_support'               => 'Do the decision-makers in your family suppot you to work?',
+    'yes'                          => 'Yes',
+    'no'                           => 'No',
+    // number of females in family options
+    'number_of_females_in_family'  => 'How many females in your family are currently working on have worked in the recent past?',
+    'none'                         => 'None',
+    'one'                          => '1',
+    'two'                          => '2',
+    'more'                         => 'If more, specify',
+    // follow up questionnaire
+    'follow_up_qes'                => 'Followup Questionnaire',
+    'other-contact_info'           => 'Other Contact Information',
+    'current_home_add'             => 'current Home Address',
+    'emergency_contact_wie'        => 'Emergency Contact',
+    'neighborhood'                 => 'Neighborhood',
+    'district'                     => 'District',
+    'province'                     => 'Province',
+    // Educational information options
+    'educational_information'      => 'Educational Information',
+    'high_school_edu'              => 'High School',
+    'university_edu'               => 'University (bachelor)',
+    'university_master'            => 'University (Master)',
+    'name_of_school'               => 'Name of School / College / University',
+    'city_location'                => 'City, Location, Country',
+    'area_of_study'                => 'Area of study / Major',
+    'start_date'                   => 'Start Date',
+    'end_date'                     => 'End Date',
+    'graduated'                    => 'Graduated',
+    // Employment Information options
+    'employment'                   => 'Employment',
+    'are_you_currently_employed'   => 'Are you currently employed?',
+    'if_yes'                       => 'If yes, what of job do you have now? (check the most appropriate) if no, skipe the employment section.',
+    'work_experience'              => 'Do you have previous work experience? If yes, please, fill in the following working experiences table and attach your CV here. If no, skip to next',
+    'company_name'                 => 'Company / Employer Name',
+    'city_or_location'             => 'City or location',
+    'position'                     => 'Position or job title',
+    'profession'                   => 'Profession',
+    'sector'                       => 'Sector',
+    'key_responsibilities'         => 'Key Responsibilities',
+    // work experience options
+    'work_experience_years'        => 'How many years total work experience do you have?',
+    'entry_level_0'                => 'Entry Level (0-1 year)',
+    'junior_level_1'               => 'Junior Level (1-3 Years)',
+    'mid_level_4'                  => 'Mid-level (4-5 years)',
+    'senior_level_5'               => 'Senior Level (5+ years)',
+    //job seeker (for all enrollments) options
+    'job_seekers'                  => 'Job seeker (for all enrollments)',
+    'industry_interested_work'     => 'What industry are you interested to work in?',
+    'Private_sector_business'      => 'Private Sector Business',
+    'banking_and_finance'          => 'Banking and Finance',
+    'government'                   => 'Government',
+    'healthcare'                   => 'HealthCare',
+    'ict'                          => 'ICT',
+    'food_processing'              => 'Agribusiness / Food Processing',
+    'own_business'                 => 'Own Business /self_employed',
+    'education_section'            => 'Education Sector',
+    'other_specify'                => 'Other (please specify)',
+    // expected salary range
+    'expected_salary'              => 'What is your expected salary range, per month (AFN)?',
+    'entry_level_range'            => 'Entry Level',
+    'mid_level_range'              => 'Mid-Level',
+    'senior_level_range'           => 'Senior Level',
+    // entrepreneurship options
+    'entrepreneurship'             => 'Entrepreneurship / Self_employed',
+    'start_your_own_business'      => 'Do you think you want to start your own business and be self-employed?',
+    'yes_i_think'                  => 'If yes, then which type of training or capacity-building do you think you most need? ',
+    'business_skill'               => 'Business Skills',
+    'soft_skill'                   => 'Soft Skills',
+    'book_keeping'                 => 'BookKeeping',
+    'business_management'          => 'Business Management',
+    'accounting'                   => 'Accounting / Finance',
+    'sales'                        => 'Sales',
+    'administrative_skills'        => 'Administrative Skills',
+    'human_resource'               => 'Human Resource',
+    'marketing'                    => 'Marketing',
+    'networking'                   => 'Networking',
+    'negotiation'                  => 'Negotiation',
+    'customer_relation'            => 'Customer Relations',
+    'problem_solving'              => 'Problem Solving',
+    'creativity'                   => 'Creativity & innovation',
+    'communication'                => 'Communication',
+    'time_management'              => 'Time Management',
+    // Technical Trainings
+
+    'technical_training'           => 'Technical Training',
+    'need_of_technical_training'   => 'Is there some specific kind of technical training or cretification you need to get a job or advance into your preferred job?',
+    'if_no_skip'                   => 'If yes, please specify. If no, skip to the next',
+    // motivational statement options
+    'motivational_statement'       => 'Motivational Statement',
+    'motivation_wants'             => 'Tell us your motivation for wanting to participate in this program. (one paragraph limit)',
+    // Time availability
+    'time_availability'            => 'Time Availability',
+    'time_form_label'              => 'How much time can you invest in your education and job search per week?',
+    'noting'                       => 'Nothing',
+    // Access to financial resources
+    'access_to_financial_res'      => 'Access to Financial Resources',
+    'financial_res_label'          => 'What financial resources do you have to invest in you career advancement and job search?',
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+];
